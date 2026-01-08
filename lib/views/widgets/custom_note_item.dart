df484c8891c7fdabaab3ae386e734e77644fae6b
Mohamed <mk2020mohamed@gmail.com>
2026-01-08 13:24:24 +0200

    create custom note item

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: const Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: const Text(
+              'Flutter tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'build your career with mohamed khaled build your career with mohamed khaled',
+                style: TextStyle(
+                  color: Colors.black.withOpacity(0.5),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.delete, color: Colors.black, size: 30),
+              onPressed: () {},
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'May 10, 2023',
+              style: TextStyle(
+                color: Colors.black.withOpacity(0.5),
+                fontSize: 16,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
