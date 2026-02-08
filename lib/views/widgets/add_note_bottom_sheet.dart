8c6a2dce1c69d98c78ff7ebdf4be4a768009ddaa
Mohamed <mk2020mohamed@gmail.com>
2026-02-08 14:14:03 +0200

    create add note cubit

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -8,7 +8,7 @@ class AddNotedBottmeSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
 
       child: SingleChildScrollView(child: AddNoteForm()),
     );
